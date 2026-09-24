8b7c2ab5c9bcf641d1cdb411baa2a40f1b00ffef
juanceballos9265 <juanceballos1153372@correo.itm.edu.co>
2026-09-24 23:37:14 +0000

    Modificación codigo carta

--- src/Carta.cpp
+++ src/Carta.cpp
@@ -9,7 +9,7 @@ private:
     int _valor;
 
 public:
-    Carta(std::string color = "", int valor = 0) : _color(color), _valor(valor) {}
+    Carta(std::string color, int valor) : _color(color), _valor(valor) {}
 
     std::string getColor() const { return _color; }
     int getValor() const { return _valor; }
